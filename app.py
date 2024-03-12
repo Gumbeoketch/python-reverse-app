a6e54bdafb51e6fd5fa0c69e5d9a97a88f9061c7
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-03-12 15:03:01 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, request
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_ip():
+    ip_address = request.remote_addr
+    reversed_ip = ".".join(ip_address.split(".")[::-1])
+    return f"Reversed IP: {reversed_ip}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
