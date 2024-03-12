4a90365aeb9256b48d86dbd8d801a5c1dcd2f44e
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-03-13 00:41:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def get_reversed_ip():
-    # Get the IP address of the client making the request
+def get_ip():
     ip_address = request.remote_addr
-    
-    # Reverse the IP address
-    reversed_ip = '.'.join(ip_address.split('.')[::-1])
-    
-    # Return the reversed IP address as the response
+    reversed_ip = ".".join(ip_address.split(".")[::-1])
     return f"Reversed IP: {reversed_ip}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
