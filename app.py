04e183dacddb1bf06f3bcb6ae02df04cdf4a7c71
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-03-13 23:08:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request #we use flask to build the app
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_ip():
-    ip_address = request.remote_addr
+    ip_address = request.remote_addr #picks the actual origin IP
     reversed_ip = ".".join(ip_address.split(".")[::-1])
     return f"Reversed IP: {reversed_ip}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) 
