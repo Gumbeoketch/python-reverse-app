78f0df47ca4cec7a52acdad2aee418548e9a7099
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-03-13 11:54:27 +0300

    Update server.tf

--- scripts/server.tf
+++ scripts/server.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  #   access_key = ""
+  #   secret_key = ""
+}
 
+resource "aws_instance" "intro" {
+  ami                    = "ami-0cd59ecaf368e5ccf"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = "#your_key"
+  vpc_security_group_ids = ["#your_SG"]
+  tags = {
+    Name    = "python-app-instance"
+    Project = "Take-Home"
