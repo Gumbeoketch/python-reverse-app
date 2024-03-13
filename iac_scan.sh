7ff42d475f24f6142ad954380d483d836263b52a
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-03-13 12:05:44 +0300

    Update iac_scan.sh

--- iac_scan.sh
+++ iac_scan.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Bash script that user checkov docker image to scan for IAC misconfigurations
 # Clone the repository containing Terraform files
-git clone <github_repository_url> /tmp/terraform_files
-
+git clone https://github.com/Gumbeoketch/python-reverse-app.git > /tmp/terraform_files
 # Run Checkov Docker image to scan Terraform files
-docker run -v /tmp/terraform_files:/tf bridgecrew/checkov --directory /tf
+docker run --rm -v /tmp/terraform_files:/tf bridgecrew/checkov:latest checkov -d /tf -o json > checkov_results.json
